cfc479e23111e847f95984c6e5428b53ee9b6f7f
Jim Junior <jimjunior854@gmail.com>
2022-01-31 15:01:45 +0300

    Update api.md

--- src/pages/docs/api.js
+++ src/pages/docs/api.js
@@ -2,7 +2,7 @@ import Orbiton from "orbiton"
 import NavBar from "../../components/index/Navbar"
 import Container from "../../components/shared/Container.jsx"
 import Head from "../../components/shared/head"
-import Docs from "../../docs/api.md"
+import Docs from "../../docs/API.md"
 
 const App = <div>
   <Head title="Library API Reference | Orbiton JS " />
