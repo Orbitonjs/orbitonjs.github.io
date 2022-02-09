99d2a00e63f68e21498c1159e32322af46740a53
Beingana Jim Junior <69729988+jim-junior@users.noreply.github.com>
2022-02-09 13:17:35 +0300

    Update index.js

--- src/pages/learn/index.js
+++ src/pages/learn/index.js
@@ -1,6 +1,6 @@
 import Orbiton from "orbiton"
 import { Container } from "../../components/docs/Containor.jsx"
-import Head from "../../../components/shared/head"
+import Head from "../../components/shared/head"
 import Docs from "../../learn/index.md"
 
 const App = <div>
